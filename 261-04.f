3f2a7879b01eda751191ed38a9a2f4ee039a8fc3
Rupert Lane <rupert@rupert-lane.org>
2024-11-03 13:17:48 +0900

    Initial version

---
+++ 261-04.f
@@ -0,0 +1,98 @@
+C The following SLIP functions always yield double precision results.   SDEC0001
+      DOUBLE PRECISION AVDLEL                                           SDEC0002
+      DOUBLE PRECISION ADVLER                                           SDEC0003
+      DOUBLE PRECISION ADVLNL                                           SDEC0004
+      DOUBLE PRECISION ADVLNR                                           SDEC0005
+      DOUBLE PRECISION ADVLWL                                           SDEC0006
+      DOUBLE PRECISION ADVLWR                                           SDEC0007
+      DOUBLE PRECISION ADVSEL                                           SDEC0008
+      DOUBLE PRECISION ADVSER                                           SDEC0009
+      DOUBLE PRECISION ADVSNL                                           SDEC0010
+      DOUBLE PRECISION ADVSNR                                           SDEC0011
+      DOUBLE PRECISION ADVSWL                                           SDEC0012
+      DOUBLE PRECISION ADVSWR                                           SDEC0013
+      DOUBLE PRECISION BOT                                              SDEC0014
+      DOUBLE PRECISION DATUM                                            SDEC0015
+      DOUBLE PRECISION DELETE                                           SDEC0016
+      DOUBLE PRECISION DLS                                              SDEC0017
+      DOUBLE PRECISION DRS                                              SDEC0018
+      DOUBLE PRECISION EBX                                              SDEC0019
+      DOUBLE PRECISION ITSVAL                                           SDEC0020
+      DOUBLE PRECISION LANORM                                           SDEC0021
+      DOUBLE PRECISION MADRGT                                           SDEC0022
+      DOUBLE PRECISION MADLFT                                           SDEC0023
+      DOUBLE PRECISION NEWVAL                                           SDEC0024
+      DOUBLE PRECISION PARMT2                                           SDEC0025
+      DOUBLE PRECISION POPBOT                                           SDEC0026
+      DOUBLE PRECISION POPTOP                                           SDEC0027
+      DOUBLE PRECISION REALL                                            SDEC0028
+      DOUBLE PRECISION REED                                             SDEC0029
+      DOUBLE PRECISION SEQLL                                            SDEC0030
+      DOUBLE PRECISION SEQLR                                            SDEC0031
+      DOUBLE PRECISION SEQSL                                            SDEC0032
+      DOUBLE PRECISION SEQSR                                            SDEC0033
+      DOUBLE PRECISION SHIN                                             SDEC0034
+      DOUBLE PRECISION SQIN                                             SDEC0035
+      DOUBLE PRECISION SQOUT                                            SDEC0036
+      DOUBLE PRECISION SUBSBT                                           SDEC0037
+      DOUBLE PRECISION SUBST                                            SDEC0038
+      DOUBLE PRECISION SUBSTP                                           SDEC0039
+      DOUBLE PRECISION TOP                                              SDEC0040
+C The following SLIP functions always return cell addresses.            SDEC0041
+      INTEGER CADRGT                                                    SDEC0042
+      INTEGER CADLFT                                                    SDEC0043
+      INTEGER LNPNTR                                                    SDEC0044
+      INTEGER MADATR                                                    SDEC0045
+      INTEGER MADNBT                                                    SDEC0046
+      INTEGER MADNTP                                                    SDEC0047
+      INTEGER MADOV                                                     SDEC0048
+      INTEGER NEWBOT                                                    SDEC0049
+      INTEGER NEWTOP                                                    SDEC0050
+      INTEGER NXTLFT                                                    SDEC0051
+      INTEGER NXTRGT                                                    SDEC0052
+C The following SLIP function always yields a list attribute.           SDEC0053
+      DOUBLE PRECISION NOATVL                                           SDEC0054
+C The following SLIP functions yield flags (-1,0, or 1).                SDEC0055
+      INTEGER EQUAL                                                     SDEC0056
+      INTEGER LISTMT                                                    SDEC0057
+      INTEGER LSTEQL                                                    SDEC0058
+      INTEGER LSTPRO                                                    SDEC0059
+      INTEGER NAMTST                                                    SDEC0060
+C The following SLIP functions yield integer results.                   SDEC0061
+      INTEGER ID                                                        SDEC0062
+      INTEGER IDATUM                                                    SDEC0063
+      INTEGER INHALT                                                    SDEC0064
+      INTEGER INTGER                                                    SDEC0065
+      INTEGER IRALST                                                    SDEC0066
+      INTEGER IRARDR                                                    SDEC0067
+      INTEGER LCNTR                                                     SDEC0068
+      INTEGER LPURGE                                                    SDEC0069
+      INTEGER LSTMRK                                                    SDEC0070
+      INTEGER MRK                                                       SDEC0071
+C The following SLIP functions yield list names.                        SDEC0072
+      DOUBLE PRECISION LOCT                                             SDEC0073
+      DOUBLE PRECISION LOFRDR                                           SDEC0074
+      DOUBLE PRECISION LSSCPY                                           SDEC0075
+      DOUBLE PRECISION INLSTL                                           SDEC0076
+      DOUBLE PRECISION INLSTR                                           SDEC0077
+      DOUBLE PRECISION LDATVL                                           SDEC0078
+      DOUBLE PRECISION LIST                                             SDEC0079
+      DOUBLE PRECISION LISTAV                                           SDEC0080
+      DOUBLE PRECISION MAKEDL                                           SDEC0081
+      DOUBLE PRECISION MRKLSS                                           SDEC0082
+      DOUBLE PRECISION MRKLST                                           SDEC0083
+      DOUBLE PRECISION MTDLST                                           SDEC0084
+      DOUBLE PRECISION MTLIST                                           SDEC0085
+      DOUBLE PRECISION NAMEDL                                           SDEC0086
+      DOUBLE PRECISION NULSTL                                           SDEC0087
+      DOUBLE PRECISION NULSTR                                           SDEC0088
+      DOUBLE PRECISION RDLSTA                                           SDEC0089
+C These SLIP functions yield list readers.                              SDEC0090
+      INTEGER LRDRCP                                                    SDEC0091
+      INTEGER LRDROV                                                    SDEC0092
+      INTEGER LVLRV1                                                    SDEC0093
+      INTEGER LVLRVT                                                    SDEC0094
+C This SLIP function yields the value of an attribute.                  SDEC0095
+      DOUBLE PRECISION RS                                               SDEC0096
+C This SLIP function yields a sequence reader.                          SDEC0097
+      INTEGER SEQRDR                                                    SDEC0098
